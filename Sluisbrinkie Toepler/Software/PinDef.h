9ebbcd03b63425cb5f093b8835055de46a19c339
Niek ten Brinke <niektb95@gmail.com>
2024-01-27 09:09:32 +0100

    Create PinDef.h

---
+++ Sluisbrinkie Toepler/Software/PinDef.h
@@ -0,0 +1,49 @@
+//To convert the port names of the ATMEGA32U4 to Arduino Micro pin numbers
+#define PC7 13
+#define PD6 12
+#define PB7 11
+#define PB6 10
+#define PB5 9
+#define PB4 8
+#define PE6 7
+#define PD7 6
+#define PC6 5
+#define PD4 4
+#define PD0 3
+#define PD1 2
+#define PD3 0
+#define PD2 1
+#define PB0 17
+#define PF7 A0
+#define PF6 A1
+#define PF5 A2
+#define PF4 A3
+#define PF1 A4
+#define PF0 A5
+
+// Actual pin mappings
+#define CV4_PIN PF7
+#define CV3_PIN PF6
+#define CV2_PIN PF5
+#define CV1_PIN PF4
+
+#define OUT1A PB5
+#define OUT1B PB6
+#define TROUT1 PF1
+
+#define BUTTON1 PD6
+#define BUTTON2 PD4
+
+#define LED_BUILTIN PC7
+#define LED1 PC7
+#define LED2 PC6
+#define LED3 PD7
+#define LED_RX PB0
+#define LED_TX PD5
+
+#define TR1_PIN PE6
+#define TR2_PIN PD1
+
+#define MIDI_CTRL PB4
+#define MIDI_RX PD3
+#define MIDI_TX PD2
